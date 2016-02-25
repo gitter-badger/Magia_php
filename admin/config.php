ca1229cdd9238e557705e376f3d32ec6034f603b
Robinson Coello <robincoello@hotmail.com>
2016-02-25 16:34:25 +0100

    creando los ficheros para acerder a admin

--- admin/config.php
+++ admin/config.php
@@ -1,1 +1,1 @@
-<?php  $path_instalacion_plugins = "../latino_documentacion/www/admin"; $path_plugins = "../admin"; $path_plugins_controlador = "path_plugins_controlador"; $path_plugins_modelos = "path_plugins_modelos"; $path_plugins_vista = "path_plugins_vista"; $path_plugins_reg = "path_plugins_reg";
+<?php  $path_instalacion_plugins = "../blog/admin"; $path_plugins = "../admin"; $path_plugins_controlador = "path_plugins_controlador"; $path_plugins_modelos = "path_plugins_modelos"; $path_plugins_vista = "path_plugins_vista"; $path_plugins_reg = "path_plugins_reg";
